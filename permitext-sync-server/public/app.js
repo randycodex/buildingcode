fa515aff9928cde21a13f47f133bd94dd8330013
randy <randyrubirosa@gmail.com>
2026-08-01 12:15:40 -0400

    Align Notebook project surface

--- permitext-sync-server/public/app.js
+++ permitext-sync-server/public/app.js
@@ -41,7 +41,7 @@ import {
   saveNotebookProjectSnapshot,
   saveOfflineSyncSnapshot,
   stageNotebookImage
-} from "./offline-storage.js?v=20260801-light-notebook-v301";
+} from "./offline-storage.js?v=20260801-light-notebook-v302";
 import {
   cacheRetryablePromise,
   resolveNotebookVersionConflict,
