326502ea11369f3413ffe82795ac162f82a47db8
randy <randyrubirosa@gmail.com>
2026-08-02 13:33:09 -0400

    style: uppercase Blocknotes heading

--- permitext-sync-server/public/app.js
+++ permitext-sync-server/public/app.js
@@ -41,7 +41,7 @@ import {
   saveNotebookProjectSnapshot,
   saveOfflineSyncSnapshot,
   stageNotebookImage
-} from "./offline-storage.js?v=20260802-compact-code-groups-v410";
+} from "./offline-storage.js?v=20260802-blocknotes-uppercase-v411";
 import {
   cacheRetryablePromise,
   resolveNotebookVersionConflict,
